6205b20998d4fa3ee45eaa4c2868b73f150bfe8f
Florian Chabert <65234487+FloDevelops@users.noreply.github.com>
2025-04-29 10:54:03 +0200

    chore: reset project

--- gatsby-browser.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react"
-import type { GatsbyBrowser } from "gatsby"
-import CustomLayout from "./gatsby/wrapRootElement"
-
-// TODO - look into making this work like gatsby-node & use typescript for the
-// things that are imported/exported.
-
-export { onInitialClientRender } from "./gatsby/onInitialClientRender"
-export const wrapPageElement: GatsbyBrowser["wrapPageElement"] = CustomLayout
--- src/components/ga4/EventBuilder/ValidateEvent/schemas/eventTypes/items.ts
+++
@@ -1,6 +0,0 @@
-// Items Schema.
-// https://developers.google.com/analytics/devguides/collection/protocol/ga4/reference/events#purchase_item
-
-import {itemSchema} from "./item"
-
-export const itemsSchema = {"type": "array", "minItems": 1, "items": itemSchema}
