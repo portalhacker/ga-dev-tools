f45a4b75d599a0400ffd4bd869f2082f3360e750
Florian Chabert <65234487+FloDevelops@users.noreply.github.com>
2025-05-01 00:43:59 +0200

    chore: remove unused context comments

--- src/app/(app)/dev2/page.client.tsx
+++ src/app/(app)/dev2/page.client.tsx
@@ -1,12 +1,7 @@
 "use client";
 
 import GapiCaller from "@/components/gapi-caller.client";
-// import GapiContextProvider from "@/contexts/GapiContextProvider";
 
 export default function ClientPage() {
-  return (
-    // <GapiContextProvider>
-    <GapiCaller />
-    // </GapiContextProvider>
-  );
+  return <GapiCaller />;
 }
