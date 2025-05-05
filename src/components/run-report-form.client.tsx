4e0060f1c385cf4a80073f7f6e7b22c32a10edf3
Florian Chabert <65234487+FloDevelops@users.noreply.github.com>
2025-05-06 01:14:37 +0200

    fix: display query explorer as 2 panels and add end date input

--- src/components/run-report-form.client.tsx
+++ src/components/run-report-form.client.tsx
@@ -10,16 +10,25 @@ import {
 } from "@/utils/gapi-utils";
 
 export default function RunReportForm() {
-  const { isGapiReady, client, accessToken } = useGapiContext();
+  const { isGapiReady, accessToken } = useGapiContext();
 
   const [formState, setFormState] = useState<{
     propertyId: number | null;
-    dateRanges: gapi.client.analyticsdata.DateRange[] | null;
+    dateRanges: gapi.client.analyticsdata.DateRange[];
     dimensions: gapi.client.analyticsdata.Dimension[] | null;
     metrics: gapi.client.analyticsdata.Metric[] | null;
   }>({
     propertyId: null,
-    dateRanges: null,
+    dateRanges: [
+      {
+        startDate: new Date(new Date().getTime() - 93 * 24 * 60 * 60 * 1000)
+          .toISOString()
+          .split("T")[0],
+        endDate: new Date(new Date().getTime() - 3 * 24 * 60 * 60 * 1000)
+          .toISOString()
+          .split("T")[0],
+      },
+    ],
     dimensions: null,
     metrics: null,
   });
@@ -88,7 +97,7 @@ export default function RunReportForm() {
         return;
       }
       if (!formState.propertyId) {
-        console.error("Property ID is not set");
+        console.log("Property ID is not set");
         return;
       }
       setIsLoading((prev) => ({ ...prev, metadata: true }));
@@ -107,160 +116,173 @@ export default function RunReportForm() {
   }, [formState.propertyId]);
 
   return (
-    <div>
-      <h2 className="text-xl font-medium">Run Report Form</h2>
-      <form className="flex flex-col gap-4">
-        <select
-          value={formState.propertyId ?? ""}
-          className="border-2"
-          onChange={(e) => {
-            const newPropertyId = parseInt(e.target.value);
-            setFormState((prev) => ({ ...prev, propertyId: newPropertyId }));
-          }}
-        >
-          {isLoading.accountSummaries ? (
-            <option>Loading...</option>
-          ) : (
-            <>
-              <option value="">Select Property</option>
-              {options.properties?.map((property) => (
-                <option
-                  key={property.property?.split("/")[1]}
-                  value={property.property?.split("/")[1]}
-                >
-                  {property.accountDisplayName} - ({property.displayName})
-                </option>
-              ))}
-            </>
-          )}
-        </select>
-        <input
-          type="date"
-          className="border-2"
-          value={
-            formState.dateRanges?.[0]?.startDate ??
-            new Date().toISOString().split("T")[0]
-          }
-          onChange={(e) => {
-            const newDateRanges = [
-              {
-                startDate: e.target.value,
-                endDate: new Date(
-                  new Date(e.target.value).getTime() + 24 * 90 * 60 * 60 * 1000,
-                )
-                  .toISOString()
-                  .split("T")[0],
-              },
-            ];
-            setFormState((prev) => ({ ...prev, dateRanges: newDateRanges }));
-          }}
-          // disabled={!formState.propertyId}
-        />
-        <select
-          multiple
-          className="h-40 border-2"
-          onChange={(e) => {
-            const selectedDimensions = Array.from(e.target.selectedOptions).map(
-              (option) => ({
-                name: option.value,
-              }),
-            );
-            setFormState((prev) => ({
-              ...prev,
-              dimensions: selectedDimensions,
-            }));
-          }}
-        >
-          {isLoading.metadata ? (
-            <option>Loading...</option>
-          ) : (
-            <>
-              {options.metadata?.dimensions?.map((dimension) => (
-                <option key={dimension.apiName} value={dimension.apiName}>
-                  {dimension.apiName}
-                  {" - "}
-                  {dimension.uiName}
-                </option>
-              ))}
-            </>
-          )}
-        </select>
-        <select
-          multiple
-          className="h-40 border-2"
-          onChange={(e) => {
-            const selectedMetrics = Array.from(e.target.selectedOptions).map(
-              (option) => ({
-                name: option.value,
-              }),
-            );
-            setFormState((prev) => ({
-              ...prev,
-              metrics: selectedMetrics,
-            }));
-          }}
-        >
-          {isLoading.metadata ? (
-            <option>Loading...</option>
-          ) : (
-            <>
-              {options.metadata?.metrics?.map((metric) => (
-                <option key={metric.apiName} value={metric.apiName}>
-                  {metric.apiName}
-                  {" - "}
-                  {metric.uiName}
-                </option>
-              ))}
-            </>
-          )}
-        </select>
-        <button
-          type="button"
-          className="rounded bg-blue-500 px-4 py-2 text-white hover:cursor-pointer hover:bg-blue-600"
-          onClick={async () => {
-            if (!isGapiReady) {
-              console.error("GAPI is not ready");
-              return;
-            }
-            if (!formState.propertyId) {
-              console.error("Property ID is not set");
-              return;
-            }
-            if (!formState.dateRanges) {
-              console.error("Date ranges are not set");
-              return;
-            }
-            if (!formState.metrics) {
-              console.error("Metrics are not set");
-              return;
-            }
-            setIsLoading((prev) => ({ ...prev, runReport: true }));
-            const runReportResponse = await runGA4Report({
-              accessToken: accessToken as gapi.client.TokenObject,
-              properyId: formState.propertyId,
-              dateRanges: formState.dateRanges,
-              dimensions: formState.dimensions ?? [],
-              metrics: formState.metrics,
-            });
-            console.log("Run report response", runReportResponse);
-            setReportData(runReportResponse);
-            setIsLoading((prev) => ({ ...prev, runReport: false }));
-          }}
-        >
-          {isLoading.runReport ? "Loading..." : "Run Report"}
-        </button>
-      </form>
-      <div>
-        {isLoading.runReport && <p>Loading...</p>}
-        {!isLoading.runReport && (
+    <>
+      <div className="flex gap-6">
+        <div className="w-[50%]">
+          <form className="mb-4 flex flex-col gap-4">
+            <select
+              value={formState.propertyId ?? ""}
+              className="border-2"
+              onChange={(e) => {
+                const newPropertyId = parseInt(e.target.value);
+                setFormState((prev) => ({
+                  ...prev,
+                  propertyId: newPropertyId,
+                }));
+              }}
+            >
+              {isLoading.accountSummaries ? (
+                <option>Loading...</option>
+              ) : (
+                <>
+                  <option value="">Select Property</option>
+                  {options.properties?.map((property) => (
+                    <option
+                      key={property.property?.split("/")[1]}
+                      value={property.property?.split("/")[1]}
+                    >
+                      {property.accountDisplayName}
+                      {" --> "}
+                      {property.displayName}
+                    </option>
+                  ))}
+                </>
+              )}
+            </select>
+            <input
+              type="date"
+              className="border-2"
+              value={formState.dateRanges?.[0]?.startDate}
+              onChange={(e) => {
+                setFormState((prev) => ({
+                  ...prev,
+                  dateRanges: [
+                    {
+                      startDate: e.target.value,
+                      endDate: prev.dateRanges?.[0]?.endDate,
+                    },
+                  ],
+                }));
+              }}
+            />
+            <input
+              type="date"
+              className="border-2"
+              value={formState.dateRanges?.[0]?.endDate}
+              onChange={(e) => {
+                setFormState((prev) => ({
+                  ...prev,
+                  dateRanges: [
+                    {
+                      startDate: prev.dateRanges?.[0]?.startDate,
+                      endDate: e.target.value,
+                    },
+                  ],
+                }));
+              }}
+            />
+            <select
+              multiple
+              className="h-40 border-2"
+              onChange={(e) => {
+                const selectedDimensions = Array.from(
+                  e.target.selectedOptions,
+                ).map((option) => ({
+                  name: option.value,
+                }));
+                setFormState((prev) => ({
+                  ...prev,
+                  dimensions: selectedDimensions,
+                }));
+              }}
+            >
+              {isLoading.metadata ? (
+                <option>Loading...</option>
+              ) : (
+                <>
+                  {options.metadata?.dimensions?.map((dimension) => (
+                    <option key={dimension.apiName} value={dimension.apiName}>
+                      {dimension.uiName} ({dimension.apiName})
+                    </option>
+                  ))}
+                </>
+              )}
+            </select>
+            <select
+              multiple
+              className="h-40 border-2"
+              onChange={(e) => {
+                const selectedMetrics = Array.from(
+                  e.target.selectedOptions,
+                ).map((option) => ({
+                  name: option.value,
+                }));
+                setFormState((prev) => ({
+                  ...prev,
+                  metrics: selectedMetrics,
+                }));
+              }}
+            >
+              {isLoading.metadata ? (
+                <option>Loading...</option>
+              ) : (
+                <>
+                  {options.metadata?.metrics?.map((metric) => (
+                    <option key={metric.apiName} value={metric.apiName}>
+                      {metric.uiName} ({metric.apiName})
+                    </option>
+                  ))}
+                </>
+              )}
+            </select>
+            <button
+              type="button"
+              className={`rounded px-4 py-2 text-white ${isLoading.runReport ? "cursor-not-allowed hover:bg-gray-600" : "bg-blue-500 hover:cursor-pointer hover:bg-blue-600"}`}
+              onClick={async () => {
+                if (!isGapiReady) {
+                  console.error("GAPI is not ready");
+                  return;
+                }
+                if (!formState.propertyId) {
+                  console.error("Property ID is not set");
+                  return;
+                }
+                if (!formState.dateRanges) {
+                  console.error("Date ranges are not set");
+                  return;
+                }
+                if (!formState.metrics) {
+                  console.error("Metrics are not set");
+                  return;
+                }
+                setIsLoading((prev) => ({ ...prev, runReport: true }));
+                const runReportResponse = await runGA4Report({
+                  accessToken: accessToken as gapi.client.TokenObject,
+                  properyId: formState.propertyId,
+                  dateRanges: formState.dateRanges,
+                  dimensions: formState.dimensions ?? [],
+                  metrics: formState.metrics,
+                });
+                console.log("Run report response", runReportResponse);
+                setReportData(runReportResponse);
+                setIsLoading((prev) => ({ ...prev, runReport: false }));
+              }}
+            >
+              {isLoading.runReport ? "Loading..." : "Run Report"}
+            </button>
+          </form>
           <code>
-            <pre className="text-gray-500">
-              {JSON.stringify(formState, null, 2)}
-            </pre>
+            <pre>{JSON.stringify(formState, null, 2)}</pre>
+          </code>
+        </div>
+        {isLoading.runReport ? (
+          <p>Loading...</p>
+        ) : (
+          <code>
             <pre>{JSON.stringify(reportData, null, 2)}</pre>
           </code>
         )}
       </div>
-    </div>
+    </>
   );
 }
